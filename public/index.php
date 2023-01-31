988665f7f756b54512d1f46d571a5622b16c197d
Brandon kwamou <brandonkwamou@gmail.com>
2023-01-31 20:02:53 +0100

    Routing

--- public/index.php
+++ public/index.php
@@ -1,3 +1,40 @@
 <?php
 
-echo 'Hello from the public folder!';
+/**
+ * Front controller
+ * PHP version 7.4
+ */
+
+// echo 'Requested URL = "' . $_SERVVER['QUERY_STRING] . '"';
+
+/*
+ * Routing 
+ */
+require '../Core/Router.php';
+
+$router = new Router();
+
+// echo get_class($router);
+
+// Add the routes
+$router->add("", ['controller' => 'Home', 'action' => 'index']);
+$router->add("posts", ['controller' => 'Posts', 'action' => 'index']);
+$router->add("posts/new", ['controller' => 'Posts', 'action' => 'new']);
+
+// Display the routing table
+// echo '<pre>';
+// var_dump($router->getRoutes());
+// echo '</pre>';
+
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for URL '$url'";
+}
+
+// echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
