c0a40d5ee8cd0c46594585c7ec66353c1d099597
Brandon kwamou <brandonkwamou@gmail.com>
2023-02-01 07:45:56 +0100

    ReGex Routing

--- public/index.php
+++ public/index.php
@@ -17,14 +17,16 @@ $router = new Router();
 // echo get_class($router);
 
 // Add the routes
-$router->add("", ['controller' => 'Home', 'action' => 'index']);
-$router->add("posts", ['controller' => 'Posts', 'action' => 'index']);
-$router->add("posts/new", ['controller' => 'Posts', 'action' => 'new']);
+$router->add("", ['controller' => 'home', 'action' => 'index']);
+$router->add("posts", ['controller' => 'posts', 'action' => 'index']);
+// $router->add("posts/new", ['controller' => 'posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
 
 // Display the routing table
-// echo '<pre>';
-// var_dump($router->getRoutes());
-// echo '</pre>';
+echo '<pre>';
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
 
 // Match the requested route
 $url = $_SERVER['QUERY_STRING'];
