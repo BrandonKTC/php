e2f95be5581cf429e6a0c9317921a04ed0baacde
Brandon kwamou <brandonkwamou@gmail.com>
2023-02-04 11:38:23 +0100

    adding Model + connection to database

---
+++ App/Models/Post.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use PDO;
+use PDOException;
+
+/**
+ * Post model
+ * 
+ * PHP version 7.4
+ */
+class Post
+{
+    /**
+     * Get all the posts as an associative array
+     * 
+     * @return array
+     */
+    public static function getAll()
+    {
+        $host = 'localhost';
+        $dbname = 'mvc';
+        $username = 'root';
+        $password = 'mysql';
+
+        try {
+            $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+
+            $stmt = $db->query('SELECT id, title, content FROM posts ORDER BY created_at');
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $results;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+}
